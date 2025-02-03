32582241ac4df78cf51b8366dec24d8d8ae61535
lightdevx <khan.m.hasanuzzaman@gmail.com>
2025-02-03 22:37:17 +0600

    u

--- js/array/arrays.js
+++ js/array/arrays.js
@@ -46,3 +46,8 @@ console.log(cars); // [ 'Audi', 'Saab', 'Volvo' ]
 console.log(cars.concat(["BMW", "Mercedes"])); // [ 'Audi', 'Saab', 'Volvo', 'BMW', 'Mercedes' ]
 
 console.log(cars);
+cars.forEach((car, index) => {
+    console.log(index, car);
+});
+
+console.log(cars.map((car) => car.toUpperCase()));
