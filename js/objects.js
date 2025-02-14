ba6a14dfa8ffaaf6cade128022ae51fd76db5bdc
lightdevx <khan.m.hasanuzzaman@gmail.com>
2025-02-14 12:55:31 +0600

    Filtering letter

--- js/objects.js
+++ js/objects.js
@@ -26,3 +26,44 @@ const colors = {
 
 console.log(colors['golden rod']);
 
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+
+const marks = student.physics.marks;
+
+console.log(marks);
+
+
+let studentProfile = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+
+const studentInfo = Object.keys(studentProfile).length;
+
+console.log(studentInfo);
+
+let myObject = {
+
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+};
+
+
+for (let key in myObject) {
+    console.log(key + '|' + typeof (myObject[key]));
+}
+
+
+
--- js/string.js
+++ js/string.js
@@ -7,3 +7,12 @@ const cityInfo = `${city} is a rural town in Rangpur Division, ${country}.`;
 const revcityInfo = cityInfo.split('').reverse().join('');  // reverse the string
 
 console.log(revcityInfo);
+
+
+const alphabetSet = "abcdefghijklmnopqrsahjaskdfsdkjfasjkdfnmsdcbkjsadfioeruehfjsdkfnsjkweuhrehfjknfakjsmrfeafnmssdfkjewruwerfhhfsduerjkaaajjasddjfsdheuerfnjcvstuvwxyz";
+const alphabetArray = alphabetSet.split('');
+
+// Filter vowels
+const vowels = alphabetArray.filter(letter => ['a', 'e', 'i', 'o', 'u'].includes(letter));
+
+console.log(vowels.length);
