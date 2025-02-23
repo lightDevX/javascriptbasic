df2bf6f8ae8555a7e68e20555e35022473edf711
lightdevx <khan.m.hasanuzzaman@gmail.com>
2025-02-23 17:20:11 +0600

    create counting func

--- js/functions/parametars.js
+++ js/functions/parametars.js
@@ -6,3 +6,21 @@ function sum(a, b) {
 
 sum(5, 10);
 
+
+
+const binaryString = '101010101000111110000111';
+
+function countZero() {
+    let count = 0;
+
+    for (let i = 0; i < binaryString.length; i++) {
+        if (binaryString[i] === '0') {
+            count++;
+        }
+    }
+    return count;
+}
+
+const result = countZero(binaryString);
+
+console.log(result);
