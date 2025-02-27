0ef0a6bc985aa23d937a2906df2e9f56a10486ad
lightdevx <khan.m.hasanuzzaman@gmail.com>
2025-02-27 22:28:05 +0600

    working comment box

--- js/events/events.js
+++ js/events/events.js
@@ -1,1 +1,10 @@
-console.log('events.js loaded');
+document.getElementById('btn-post').addEventListener('click', function () {
+    const commentBox = document.getElementById('new-comment');
+    const comment = commentBox.value;
+    const commentContainer = document.getElementById('comment-container');
+    const p = document.createElement('p');
+    p.innerText = comment;
+    commentContainer.appendChild(p);
+    commentBox.value = '';
+
+});
