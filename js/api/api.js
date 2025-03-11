e4ac89db15aa4a2f901ac0041e5b82792a91a4b3
lightdevx <khan.m.hasanuzzaman@gmail.com>
2025-03-11 16:52:07 +0600

    user list fetched

--- js/api/api.js
+++ js/api/api.js
@@ -1,13 +1,16 @@
-const url = 'https://jsonplaceholder.typicode.com/users';
-
-
-
 function loadUserData() {
+    const url = 'https://jsonplaceholder.typicode.com/users';
     fetch(url)
         .then(res => res.json())
         .then(data => display(data))
+        .catch(error => console.error('Error fetching data:', error));
 }
 
-function display(users) {
-    console.log('Loading Users', users);
+function display(data) {
+    const ul = document.getElementById('user-list');
+    for (const user of data) {
+        const li = document.createElement('li');
+        li.innerText = user.name;
+        ul.appendChild(li);
+    }
 }
