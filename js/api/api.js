6bb6d63d063e99a9ba793e3b75a91b8fc40cb741
lightdevx <khan.m.hasanuzzaman@gmail.com>
2025-03-12 10:50:47 +0600

    Includes async and await

--- js/api/api.js
+++ js/api/api.js
@@ -14,3 +14,46 @@ function display(data) {
         ul.appendChild(li);
     }
 }
+
+
+function loadPostData() {
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPost(data))
+        .catch(err => console.error('Error fetching data:', err));
+}
+
+function displayPost(posts) {
+    const div = document.getElementById('post-list');
+    for (const post of posts) {
+        console.log(post);
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML = `
+        <h3>User: ${post.id}</h3>
+        <p>Title:${post.title}</p>
+        <p>Post: ${post.body}</p>
+        `
+        div.appendChild(newDiv);
+    }
+}
+
+
+
+
+
+// async and await
+
+const loadComments = async () => {
+    try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/comments');
+        const data = await res.json();
+        displayComments(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const displayComments = (data) => {
+    console.log(data);
+}
